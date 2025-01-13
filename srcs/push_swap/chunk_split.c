883e14e59cb3f75dc8231cef9737b3bfe75fffe4
Dong Hyun Kim <dong-hki@c3r20s4.42gyeongsan.kr>
2025-01-13 13:23:46 +0900

    Jan 13, 2025

---
+++ srcs/push_swap/chunk_split.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunk_split.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dong-hki <dong-hki@student.42gyeongsan.kr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 12:46:50 by dong-hki          #+#    #+#             */
+/*   Updated: 2025/01/13 13:23:29 by dong-hki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	init_size(t_chunk *min, t_chunk *mid, t_chunk *max)
+{
+	min->size = 0;
+	mid->size = 0;
+	max->size = 0;
+}
+
+void	set_split_loc(enum e_loc loc, t_chunk *min, t_chunk *mid, t_chunk *max)
+{
+	if (loc == TOP_A)
+	{
+		min->loc = BOTTOM_B;
+		mid->loc = TOP_B;
+		max->loc = BOTTON_A;
+	}
+	else if (loc == TOP_B)
+	{
+		min->loc = BOTTOM_B;
+		mid->loc = BOTTOM_A;
+		max->loc = TOP_A;
+	}
+	else if (loc == BOTTOM_A)
+	{
+		min->loc = BOTTOM_B;
+		mid->loc = TOP_B;
+		max->loc = TOP_A;
+	}
+	else if (loc == BOTTOM_B)
+	{
+		min->loc = TOP_B;
+		mid->loc = BOTTOM_A;
+		max->loc = TOP_A;
+	}
+}
+
+void	set_third_pivot(enum e_loc loc, int size, int *pivot_1, int *pivot_2)
+{
+	*pivot_2 = size / 3;
+	if (loc == TOP_A || loc == BOTTOM_A)
+		*pivot_1 = 2 * pivot_2;
+	if (loc == TOP_B || loc == BOTTOM_B)
+		*pivot_1 = size / 2;
+	if ((loc == TOP_A || loc == BOTTOM_A) && size < 15)
+		*pivot_1 = size;
+	if (loc == BOTTOM_B && size < 8)
+		*pivot_1 = size / 2;
+}
+
+void	
