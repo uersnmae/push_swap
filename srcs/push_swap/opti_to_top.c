59c897e08e8d19d83fb74c9a89e0d868700992cd
Dong Hyun Kim <dong-hki@c1r12s6.42gyeongsan.kr>
2025-01-22 13:42:11 +0900

    Jan 22, 2025

--- srcs/push_swap/opti_to_top.c
+++ srcs/push_swap/opti_to_top.c
@@ -6,7 +6,7 @@
 /*   By: dong-hki <dong-hki@student.42gyeongsan.kr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:51:27 by dong-hki          #+#    #+#             */
-/*   Updated: 2025/01/14 15:21:21 by dong-hki         ###   ########.fr       */
+/*   Updated: 2025/01/22 10:09:04 by dong-hki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	chunk_to_top(t_ps *data, t_chunk *to_sort)
 {
 	if (to_sort->loc == BOTTOM_B && curr_size(&data->b) == to_sort->size)
 		to_sort->loc = TOP_B;
-	if (to_sort->loc == BOTTOM_A && curr_size(&data->a) == to_sort->size)
+	else if (to_sort->loc == BOTTOM_A && curr_size(&data->a) == to_sort->size)
 		to_sort->loc = TOP_A;
 }
